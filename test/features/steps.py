93fd2cc3fb12b9fc744dec3d1999cc2e60ced501
Andy <zongbo.chen@ge.com>
2019-01-09 00:22:28 +0800

    add function

--- test/features/steps.py
+++ test/features/steps.py
@@ -2,28 +2,23 @@
 from lettuce import step, world, before
 from rest_api_demo.app import app
 import json
+import requests
 
-@before.all
-def before_all():
-    world.app = app.test_client()
+# @before.all
+# def before_all():
+#     requests.
 
 
 
 @step(u'When I retrieve data from blog api')
 def when_i_retrieve_data_from_blog_api(step):
-    assert hasattr(world.app, 'get')
-    world.response = world.app.get("/")
-    print "dafds" 
+    world.response = requests.get("http://localhost:8888/api/blog/posts/?per_page=2&page=1")
 
 @step(u'Then I should get a \'([^\']*)\' status code')
 def then_i_should_get_a_group1_status_code(step, expectedCode):
-    print world.response.status_code
-    print expectedCode
-    assert(int(expectedCode) == 200)
-    assert(hasattr(world, 'response'))
-    assert world.response.status_code == int(200), "correct status code" 
+    assert world.response.status_code == int(expectedCode), "correct status code" 
 
 
 @step(u'Then the following info are returned:')
 def then_the_following_info_are_returned(step):
-    assert world.response.data == "Hello, World!" , 'This step must be implemented'
+    assert world.response.json().items == step.hashes , 'This step must be implemented'
--- test/features/test.py
+++ test/features/test.py
@@ -3,8 +3,8 @@ from lettuce import step, world, before
 from rest_api_demo.app import app
 
 from rest_api_demo.api.restplus import api
+import requests
 
 if __name__ == "__main__":
-    client = app.test_client()
-    # response = client.get("/blog/categories/")
-    doc(api)
+     response = requests.get("http://localhost:8888/api/blog/posts/?per_page=2&page=1")
+     print response.json().items()
